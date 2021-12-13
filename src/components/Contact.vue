7937f866e1537018cc268f59aac20c43b36d6464
chaosfreak93 <yanluca.lentzen@gmail.com>
2021-12-13 12:46:34 +0100

    Contact Prototype

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="contact">
     <h1>Kontakt</h1>
-    <input type="text" placeholder="Vorname" />
-    <input type="text" placeholder="Nachname" />
-    <input
-      type="email"
-      placeholder="E-Mail"
-      required="required"
-      spellcheck="true"
-    />
-    <textarea placeholder="Nachricht hier..." />
+    <form>
+      <input type="text" placeholder="Vorname" required="required" />
+      <input type="text" placeholder="Nachname" required="required" />
+      <input
+        type="email"
+        placeholder="E-Mail"
+        required="required"
+        spellcheck="true"
+      />
+      <textarea placeholder="Ihre Nachricht hier..." required="required" />
+      <button type="submit"></button>
+    </form>
   </div>
 </template>
 
