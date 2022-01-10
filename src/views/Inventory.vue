effcb0fe400a444f4afd9cfe47e6666ec52deeb9
chaosfreak93 <yanluca.lentzen@gmail.com>
2022-01-10 12:52:24 +0100

    Product Images

--- src/views/Inventory.vue
+++ src/views/Inventory.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="inventory">
-      <ul v-if="data">
-        <li v-for="product in data.products" :key="product.name">
-            {{ product.name }}, {{ product.price }}
-        </li>
-      </ul>
+    <ul v-if="data">
+      <li v-for="product in data.products" :key="product.name">
+        {{ product.name }}, {{ product.price }}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -15,8 +15,7 @@ export default {
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   setup() {
     const { data } = useQuery({
-      query:
-        "{ products { name, price } }",
+      query: "{ products { name, price } }",
     });
     return { data };
   },
