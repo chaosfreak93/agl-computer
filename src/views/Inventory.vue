23a1ad57ecd794f7025e307b930b7db44361092f
chaosfreak93 <yanluca.lentzen@gmail.com>
2022-01-13 13:50:37 +0100

    First working product list

--- src/views/Inventory.vue
+++ src/views/Inventory.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="inventory">
     <ul v-if="data">
-      <li v-for="product in data.products" :key="product.name">
-        {{ product.name }}, {{ product.price }}
+      <li v-for="product in data.products" :key="product._id">
+        <img
+          v-if="product.image"
+          id="image"
+          :src="this.$store.state.cdnUrl + 'products/' + product.image"
+          height="64"
+          width="64"
+        />
+        <div id="name">{{ product.name }}</div>
+        <div id="price">{{ product.price }}€</div>
       </li>
     </ul>
   </div>
@@ -15,7 +23,7 @@ export default {
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   setup() {
     const { data } = useQuery({
-      query: "{ products { name, price } }",
+      query: "{ products { _id, name, price, image } }",
     });
     return { data };
   },
