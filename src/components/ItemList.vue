ee0ddfc4599fb19d5b88323601ddd884f3e5523c
chaosfreak93 <yanluca.lentzen@gmail.com>
2022-01-19 09:50:55 +0100

    Started work on ItemList Component

---
+++ src/components/ItemList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="itemlist" v-if="list">
+    <div class="item" v-for="item in list" :key="item._id">
+      <div id="id">{{ item._id }}</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+
+export default class ItemList extends Vue {
+    data() {
+        return {
+            list: {}
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss"></style>
