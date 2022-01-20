aedc99497b6b70b6d954835d94a59b7ad1be2ac4
chaosfreak93 <yanluca.lentzen@gmail.com>
2022-01-20 11:25:01 +0100

    Reworked Product Site

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,2 +1,28 @@
 <template>
+  <Header title="Unsere Produkte."></Header>
+  <div class="products">
+    <ProductGrid></ProductGrid>
+  </div>
 </template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import Header from "@/components/Header.vue";
+import ProductGrid from "@/components/ProductGrid.vue";
+
+@Options({
+  components: {
+    Header,
+    ProductGrid,
+  },
+})
+export default class Products extends Vue {}
+</script>
+
+<style lang="scss">
+@import "@/assets/theme";
+
+.products {
+  background: $color3;
+}
+</style>
